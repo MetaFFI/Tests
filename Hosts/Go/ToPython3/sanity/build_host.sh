3c1a6d6ad8491160de64e5ea750005830798c8c5
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-04-05 09:41:06 +0300

    use host-option for go

--- Hosts/Go/ToPython3/sanity/build_host.sh
+++ Hosts/Go/ToPython3/sanity/build_host.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo openffi -c --idl Test.proto -f go
-openffi -c --idl Test.proto -f go
+echo openffi -c --idl Test.proto -f go --host-options "package=sanity"
+openffi -c --idl Test.proto -f go --host-options "package=sanity"
