fa2799b6d2e5607346f18339480a9511285a1b86
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-04-26 21:29:57 +0300

    python3 to scala pass

---
+++ Hosts/Python3/ToScala/sanity/run_test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo Copying Scala test code
+cp ../../../../Guests/Scala/sanity/build_guest.sh .
+cp ../../../../Guests/Scala/sanity/Test.proto .
+cp ../../../../Guests/Scala/sanity/TestFuncs.scala .
+
+echo building guest
+./build_guest.sh
+
+echo building host
+./build_host.sh
+
+echo running tests
+python3 -m unittest Main_test.TestSanity
+
+echo Deleting guest files
+rm build_guest.sh
+rm Test.proto
+rm TestFuncs.scala
+rm Test_OpenFFIGuest.jar
+rm *.class
+
+echo Delete host file
+rm Test_OpenFFIHost_pb2.py
+
+echo Done Go to Scala
