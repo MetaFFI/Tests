c1fe9210f478cd21a7c1629d630118ff4d7d7011
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-04-26 21:26:28 +0300

    python3 to groove pass

---
+++ Hosts/Python3/ToGroovy/sanity/run_test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo Copying Groovy test code
+cp ../../../../Guests/Groovy/sanity/build_guest.sh .
+cp ../../../../Guests/Groovy/sanity/Test.proto .
+cp ../../../../Guests/Groovy/sanity/TestFuncs.groovy .
+
+echo building guest
+./build_guest.sh
+
+echo building host
+./build_host.sh
+
+echo running tests
+python3 -m unittest Main_test.TestSanity
+
+echo Deleting guest files
+rm build_guest.sh
+rm Test.proto
+rm TestFuncs.groovy
+rm Test_OpenFFIGuest.jar
+
+echo Delete host file
+rm Test_OpenFFIHost_pb2.py
+rm TestFuncs.class
+
+echo Done Go to Groovy
