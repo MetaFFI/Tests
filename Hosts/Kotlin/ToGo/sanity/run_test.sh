1d758604d4628683384996f5624a0ec42d5b4db1
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-04-26 10:25:21 +0300

    Kotlin to go pass

---
+++ Hosts/Kotlin/ToGo/sanity/run_test.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo Copying go test code
+cp ../../../../Guests/Go/sanity/build_guest.sh .
+cp ../../../../Guests/Go/sanity/go.mod .
+cp ../../../../Guests/Go/sanity/Test.proto .
+cp ../../../../Guests/Go/sanity/TestFuncs.go .
+
+echo building guest
+./build_guest.sh
+
+echo building host
+./build_host.sh
+
+echo Compiling Test Code
+kotlinc -cp "Test_OpenFFIHost.jar:$OPENFFI_HOME/xllr.openjdk.bridge.jar:$OPENFFI_HOME/protobuf-java-3.15.2.jar" Main_test.kt
+
+echo running tests
+kotlin -cp ".:./sanity:Test_OpenFFIHost.jar:$OPENFFI_HOME/xllr.openjdk.bridge.jar:$OPENFFI_HOME/protobuf-java-3.15.2.jar" Main_testKt
+echo Tests ran successfully!
+
+echo Starting cleanup...
+
+echo Deleting guest files
+rm build_guest.sh
+rm go.mod
+rm Test.proto
+rm TestFuncs.go
+rm Test_OpenFFIGuest.so
+
+echo Delete host file
+rm Test_OpenFFIHost.jar
+rm Main_testKt.class
+rm -r META-INF
+
+echo Done Java to Go
