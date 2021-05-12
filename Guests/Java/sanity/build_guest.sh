1eb518f22c6199c8d0df25a44c491e9cd14519c3
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-05-12 13:45:57 +0300

    groovy to scala ; groovy to java pass

--- Guests/Java/sanity/build_guest.sh
+++ Guests/Java/sanity/build_guest.sh
@@ -2,7 +2,7 @@
 
 # build java code containing foreign functions
 echo javac TestFuncs.java
-javac TestFuncs.java
+javac TestFuncs.java -d .
 
 echo openffi -c --idl Test.proto -t
 openffi -c --idl Test.proto -t
--- Guests/Scala/sanity/build_guest.sh
+++ Guests/Scala/sanity/build_guest.sh
@@ -2,7 +2,7 @@
 
 # build java code containing foreign functions
 echo scalac TestFuncs.scala
-scalac TestFuncs.scala -d ..
+scalac TestFuncs.scala -d .
 
 echo openffi -c --idl Test.proto -t
 openffi -c --idl Test.proto -t
