395d1415a0a7a7074a0c49dfa89c161a8d1513e6
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-05-12 16:35:43 +0300

    Done Kotlin to groovy

--- Guests/Groovy/sanity/build_guest.sh
+++ Guests/Groovy/sanity/build_guest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # build java code containing foreign functions
-echo groovyc TestFuncs.groovy -d ..
-groovyc TestFuncs.groovy -d ..
+echo groovyc TestFuncs.groovy -d .
+groovyc TestFuncs.groovy -d .
 
 echo openffi -c --idl Test.proto -t
 openffi -c --idl Test.proto -t
