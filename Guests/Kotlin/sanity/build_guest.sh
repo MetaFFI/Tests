531675af9139a00f576a041855ed5ac5d202b4c5
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-05-12 13:26:46 +0300

    groovy to kotlin pass

--- Guests/Kotlin/sanity/build_guest.sh
+++ Guests/Kotlin/sanity/build_guest.sh
@@ -3,7 +3,7 @@ set -e
 
 # build java code containing foreign functions
 echo kotlinc TestFuncs.kt
-kotlinc TestFuncs.kt -d ..
+kotlinc TestFuncs.kt -d .
 
 echo openffi -c --idl Test.proto -t
 openffi -c --idl Test.proto -t
--- Hosts/Groovy/ToGo/sanity/run_test.sh
+++ Hosts/Groovy/ToGo/sanity/run_test.sh
@@ -14,9 +14,6 @@ echo building guest
 echo building host
 ./build_host.sh
 
-echo Compiling Test Code
-groovyc -cp "./..:Test_OpenFFIHost.jar:$OPENFFI_HOME/xllr.openjdk.bridge.jar:$OPENFFI_HOME/protobuf-java-3.15.2.jar" Main_test.groovy
-
 echo running tests
 groovy -cp ".:Test_OpenFFIHost.jar:$OPENFFI_HOME/xllr.openjdk.bridge.jar:$OPENFFI_HOME/protobuf-java-3.15.2.jar" Main_test
 echo Tests ran successfully!
@@ -32,6 +29,5 @@ rm Test_OpenFFIGuest.so
 
 echo Delete host file
 rm Test_OpenFFIHost.jar
-rm -r sanity
 
 echo Done Groovy to Go
